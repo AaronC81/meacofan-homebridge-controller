11a92aedd2026e9faf8369b75912b3fa701d36ed
Aaron Christiansen <aaronc20000@gmail.com>
2024-02-21 21:07:04 +0000

    Terrible I2C bitbang

--- include/pins.hpp
+++ include/pins.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
 #define IR_PIN 3
+
+#define I2C_SDA_PIN 0
+#define I2C_SCL_PIN 2
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,81 @@
 #include <Arduino.h>
 
-#include "nec_ir.hpp"
+// #include "nec_ir.hpp"
+#include "pins.hpp"
+
+#include <IRremote.hpp>
 
 void setup() {
-    nec_ir::begin();
+    // nec_ir::begin();
+    
+    pinMode(IR_PIN, OUTPUT);
+
+    pinMode(I2C_SCL_PIN, INPUT_PULLUP);
+    pinMode(I2C_SDA_PIN, INPUT_PULLUP);
 }
 
 void loop() {
+    /*
     while (1) {
         nec_ir::transmit(0x80, 0x92);
         delay(2000);
     }
+    */
+
+    // If SCL goes low, we're going to receive a transmission
+    if (digitalReadFast(I2C_SCL_PIN) == 0) {
+        digitalWriteFast(IR_PIN, HIGH);
+
+        // Read address
+        uint8_t address = 0;
+        uint8_t i = 7;
+        while (1) {
+            // Wait for clock high
+            while (digitalReadFast(I2C_SCL_PIN) == 0);
+
+            // Read bit
+            address |= digitalReadFast(I2C_SDA_PIN) << i;
+
+            if (i == 0) break;
+            i--;
+
+            // Wait for clock low
+            while (digitalReadFast(I2C_SCL_PIN) == 1);
+        }
+
+        // Once we've received the address, pull down SDA to ACK, wait a cycle, and release
+        pinModeFast(I2C_SDA_PIN, OUTPUT);
+        digitalWriteFast(I2C_SDA_PIN, LOW);
+        while (digitalReadFast(I2C_SCL_PIN) == 1);
+        while (digitalReadFast(I2C_SCL_PIN) == 0);
+        while (digitalReadFast(I2C_SCL_PIN) == 1);
+        pinModeFast(I2C_SDA_PIN, INPUT_PULLUP);
+
+        // Read data
+        uint8_t data = 0;
+        i = 7;
+        while (1) {
+            // Wait for clock high
+            while (digitalReadFast(I2C_SCL_PIN) == 0);
+
+            // Read bit
+            data |= digitalReadFast(I2C_SDA_PIN) << i;
+
+            if (i == 0) break;
+            i--;
+
+            // Wait for clock low
+            while (digitalReadFast(I2C_SCL_PIN) == 1);
+        }
+
+        // Once we've received the address, pull down SDA to ACK, wait a cycle, and release
+        pinModeFast(I2C_SDA_PIN, OUTPUT);
+        digitalWriteFast(I2C_SDA_PIN, LOW);
+        while (digitalReadFast(I2C_SCL_PIN) == 1);
+        while (digitalReadFast(I2C_SCL_PIN) == 0);
+        // We don't get a return to high this time
+        pinModeFast(I2C_SDA_PIN, INPUT_PULLUP);
+    }
+
+    digitalWriteFast(IR_PIN, LOW);
 } 
