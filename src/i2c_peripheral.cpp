a9f269ee5ef9cc3dbf9b2dbc1caa0830601677cf
Aaron Christiansen <aaronc20000@gmail.com>
2024-02-22 22:44:58 +0000

    Add a crap timer to bail out of broken transmission
    This does actually work; interrupt-based hardware timers would be way
    better, but this was much easier to implement :P

--- src/i2c_peripheral.cpp
+++ src/i2c_peripheral.cpp
@@ -12,22 +12,36 @@ namespace i2c_peripheral {
     }
 
     // Convenience macros. Don't want to risk the overhead of a non-inlined function for these!
-    #define WAIT_FOR_CLOCK_HIGH while (digitalReadFast(I2C_SCL_PIN) == 0);
-    #define WAIT_FOR_CLOCK_LOW  while (digitalReadFast(I2C_SCL_PIN) == 1);
+    #define WAIT_FOR_CLOCK_HIGH \
+        while (1) { \
+            if (digitalReadFast(I2C_SCL_PIN)) break; \
+            if (millis() - start_time > 5) return false; \
+        }
+    #define WAIT_FOR_CLOCK_LOW \
+        while (1) { \
+            if (!digitalReadFast(I2C_SCL_PIN)) break; \
+            if (millis() - start_time > 5) return false; \
+        }
 
     bool wait_for_byte(uint8_t *data_ptr) {
+        // If the timer made us bail, we can get stuck in a weird state (e.g. where we're stuck
+        // holding the bus high for an ACK or something).
+        // To be safe, re-init our pins.
+        begin();
+
         while (1) {
             // If SCL goes low, we're going to receive a transmission
-            // TODO: set up a timer to get us back to a nice state if things go awry
             if (digitalReadFast(I2C_SCL_PIN) == 0) {
+                unsigned long start_time = millis();
+
                 // Read address
-                uint8_t addressAndMode = 0;
+                uint8_t address_and_mode = 0;
                 uint8_t i = 7;
                 while (1) {
                     WAIT_FOR_CLOCK_HIGH;
 
                     // Read bit
-                    addressAndMode |= digitalReadFast(I2C_SDA_PIN) << i;
+                    address_and_mode |= digitalReadFast(I2C_SDA_PIN) << i;
 
                     if (i == 0) break;
                     i--;
@@ -37,7 +51,7 @@ namespace i2c_peripheral {
 
                 // The byte transmission we just received actually encodes two data items; the first
                 // 7 bits is the address, and the last bit is write (0) or read (1)
-                uint8_t address = addressAndMode >> 1;
+                uint8_t address = address_and_mode >> 1;
 
                 // Once we've received the address, pull down SDA to ACK, wait a cycle, and release
                 if (address != I2C_ADDRESS)
