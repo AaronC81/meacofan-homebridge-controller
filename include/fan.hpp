ed3253a929b39c2a17d4a88df702e10b1497517a
Aaron Christiansen <aaronc20000@gmail.com>
2024-02-25 22:56:36 +0000

    Handle sleep mode

--- include/fan.hpp
+++ include/fan.hpp
@@ -2,8 +2,6 @@
 
 #include <stdint.h>
 
-// TODO: logic for standby
-
 #include <nec_ir.hpp>
 
 /**
@@ -16,12 +14,40 @@ public:
     const uint8_t COMMAND_POWER = 0x92;
     const uint8_t COMMAND_SPEED_UP = 0x89;
     const uint8_t COMMAND_SPEED_DOWN = 0x9F;
+    const uint8_t COMMAND_LIGHT = 0x9E;
+
+    /**
+     * @brief The amount of time before the fan goes into sleep mode while turned on, requiring
+     *        an additional button press to wake up first.
+     */
+    const unsigned long SLEEP_TIMEOUT_MS = 60000;
+
+    /**
+     * @brief Send a command to the fan, accounting for any special requirements for sleep mode. 
+     * 
+     * @param command The IR data byte to transmit.
+     */
+    void transmit(uint8_t command) {
+        if (on && millis() - last_interaction_millis > SLEEP_TIMEOUT_MS) {
+            // The fan is asleep! Wake it up by sending LIGHT.
+            // This isn't tracked as part of the state, so if we're slightly off with our timing,
+            // there's no noticeable side effects on our part.
+            nec_ir::transmit(ADDRESS, COMMAND_LIGHT);
+
+            delay(50); // Wait a little bit, just in case
+        }
+
+        nec_ir::transmit(ADDRESS, command);
+
+        // Update last interaction time, so we know if we're asleep by the next interaction
+        last_interaction_millis = millis();
+    }
 
     /**
      * @brief Toggle the fan's power, and update the model accordingly.
      */
     void toggle_power() {
-        nec_ir::transmit(ADDRESS, COMMAND_POWER);
+        transmit(COMMAND_POWER);
         on = !on;
     }
 
@@ -29,7 +55,7 @@ public:
      * @brief Increase the fan's speed, and update the model accordingly.
      */
     void speed_up() {
-        nec_ir::transmit(ADDRESS, COMMAND_SPEED_UP);
+        transmit(COMMAND_SPEED_UP);
         if (current_speed != 12) current_speed++;
     }
 
@@ -37,7 +63,7 @@ public:
      * @brief Decrease the fan's speed, and update the model accordingly.
      */
     void speed_down() {
-        nec_ir::transmit(ADDRESS, COMMAND_SPEED_DOWN);
+        transmit(COMMAND_SPEED_DOWN);
         if (current_speed != 1) current_speed--;
     }
 
@@ -86,4 +112,10 @@ private:
      * @brief Whether the fan is currently powered on.
      */
     bool on = false;
+
+    /**
+     * @brief The `millis` value the last time a keypress was sent to the fan. Used to keep track of
+     *        whether it is asleep, based on `SLEEP_TIMEOUT_MS`.
+     */
+    unsigned long last_interaction_millis = 0;
 };
